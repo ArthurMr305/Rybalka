437567d88257724d84b66f3f08f4a1dba7398814
ArthurMr305 <48913930+ArthurMr305@users.noreply.github.com>
2019-03-27 13:58:17 +0200

    first

---
+++ New Unity Project/Assets/Scripts/Buttons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+	public Sprite layer_blue,layer_green;
+	public string action;
+	void OnMouseDown(){
+		GetComponent <SpriteRenderer> ().sprite = layer_blue;
+	}
+	void OnMouseUp(){
+		GetComponent <SpriteRenderer> ().sprite = layer_green;
+	}
+	void OnMouseUpAsButton(){
+
+	switch (action){
+	case"Run":
+			Application.LoadLevel("Play");
+			break;
+		case"Comment":
+			Application.OpenURL ("www.zntu.edu.ua");
+			break;
+		case"Back":
+			Application.LoadLevel ("Main");
+			break;
+		}
+
+
+}
+}
---
+++ New Unity Project/Assets/Scripts/SpawnFish.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnFish : MonoBehaviour {
+	public GameObject fish,fish1,fish2,trash,trash1;
+	void Start() {
+		StartCoroutine (Spawn ());
+			}
+	IEnumerator Spawn(){
+		while(true){
+			Instantiate (fish,new Vector2 (Random.Range (-2.5f, 2.5f), 5.9f), Quaternion.identity);
+			yield return new WaitForSeconds(1f);
+			Instantiate (fish1,new Vector2 (Random.Range (-2.5f, 2.5f), 5.9f), Quaternion.identity);
+			yield return new WaitForSeconds(1f);
+			Instantiate (fish2,new Vector2 (Random.Range (-2.5f, 2.5f), 5.9f), Quaternion.identity);
+			yield return new WaitForSeconds(1f);
+			Instantiate (trash,new Vector2 (Random.Range (-2.5f, 2.5f), 5.9f), Quaternion.identity);
+			yield return new WaitForSeconds(1f);
+			Instantiate (trash1,new Vector2 (Random.Range (-2.5f, 2.5f), 5.9f), Quaternion.identity);
+			yield return new WaitForSeconds(1f);
+			}
+	}
+}
