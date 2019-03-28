1c1b3cd1a7ca9602bbb3cf58134a435a154c11a5
ArthurMr305 <48913930+ArthurMr305@users.noreply.github.com>
2019-03-28 18:18:29 +0200

    third

---
+++ New Unity Project/Assets/Scripts/FallDownTrash.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+public class FallDownTrash : MonoBehaviour {
+	[SerializeField]
+	private float fallSpeed = 2f;
+	void Update(){
+		transform.position -=new Vector3(0,fallSpeed *Time.deltaTime,0);
+	}
+}
---
+++ New Unity Project/Assets/Scripts/SpawnBombs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnBombs : MonoBehaviour {
+	public GameObject trash, trash1, trash2,treasure;
+
+	void Start(){
+		StartCoroutine (Spawn ());
+		}
+	IEnumerator Spawn (){
+		while(true){
+			Instantiate (trash, new Vector2 (Random.Range (-1.06f, 2.51f), 5.7f), Quaternion.identity);
+			yield return new WaitForSeconds (1.3f);
+			Instantiate (trash1, new Vector2 (Random.Range (-1.06f, 2.51f), 5.7f), Quaternion.identity);
+			yield return new WaitForSeconds (1.4f);
+			Instantiate (trash2, new Vector2 (Random.Range (-1.06f, 2.51f), 5.7f), Quaternion.identity);
+			yield return new WaitForSeconds (1.2f);
+
+		}
+
+	}
+
+}
