3d64b1b8c92f79569fbc11abd9f8e1fc4fc92785
ArthurMr305 <48913930+ArthurMr305@users.noreply.github.com>
2019-03-31 22:06:45 +0300

    BestButtons

---
+++ New Unity Project/Assets/Scripts/TheSun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class TheSun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0f,0f,1f));
+	}
+}
